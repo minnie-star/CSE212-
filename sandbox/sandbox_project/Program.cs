3d621eb81504e35713ba8907d60373ab15af418f
minnie-star <minenhleminnie8@gmail.com>
2025-11-24 16:31:43 +0200

    week05

--- sandbox/sandbox_project/Program.cs
+++ sandbox/sandbox_project/Program.cs
@@ -8,6 +8,29 @@ public class Program
         // with any code or ideas you have that do not directly apply to
         // one of your projects.
 
-        Console.WriteLine("Hello Sandbox World!");
+        //Console.WriteLine("Hello Sandbox World!");
+
+        LinkedList<string> myList = new LinkedList<string>();
+        myList.AddLast("Minnie");
+        myList.AddLast("Minnky");
+        myList.AddLast("Mindy");
+        myList.AddLast("Mouse");
+
+        myList.AddFirst("Selly");
+        myList.AddFirst("Sam");
+
+        LinkedListNode<string> node = myList.Find("Mindy");
+        myList.AddAfter(node, "Mindy-2");
+
+        myList.RemoveFirst();
+
+        Counsole.WriteLine(myList.Count());
+
+        foreach(var x in myList)
+        Console.WriteLine(x);
+
+
+
+
     }
 }
--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections;
+using System.Collections.Generic;
 
 public static class Recursion
 {
@@ -15,7 +17,11 @@ public static class Recursion
     public static int SumSquaresRecursive(int n)
     {
         // TODO Start Problem 1
-        return 0;
+        if (n <= 0)
+        {
+            return 0;
+        }
+        return n * n + SumSquaresRecursive(n - 1);
     }
 
     /// <summary>
@@ -40,6 +46,19 @@ public static class Recursion
     public static void PermutationsChoose(List<string> results, string letters, int size, string word = "")
     {
         // TODO Start Problem 2
+        if (size == word.Length)
+        {
+            results.Add(word);
+            return;
+        }
+
+        for (int i = 0; i < letters.Length; i++)
+        {
+            char pick = letters[i];
+            string remaining = letters.Substring(0, i) + letters.Substring(i + 1);
+
+            PermutationsChoose(results, remaining, size, word + pick);
+        }
     }
 
     /// <summary>
@@ -97,9 +116,19 @@ public static class Recursion
             return 4;
 
         // TODO Start Problem 3
+        if (remember == null)
+        {
+            remember = new Dictionary<int, decimal>();
+        }
+
+        if (remember.ContainsKey(s))
+        {
+            return remember[s];
+        }
 
         // Solve using recursion
         decimal ways = CountWaysToClimb(s - 1) + CountWaysToClimb(s - 2) + CountWaysToClimb(s - 3);
+        remember[s] = ways;
         return ways;
     }
 
@@ -119,6 +148,18 @@ public static class Recursion
     public static void WildcardBinary(string pattern, List<string> results)
     {
         // TODO Start Problem 4
+        int idx = pattern.IndexOf('*');
+        if (idx < 0)
+        {
+            results.Add(pattern);
+            return;
+        }
+
+        string left = pattern[..idx];
+        string right = pattern[(idx + 1)..];
+
+        WildcardBinary(left + "0" + right, results);
+        WildcardBinary(left + "1" + right, results);
     }
 
     /// <summary>
@@ -133,10 +174,17 @@ public static class Recursion
             currPath = new List<ValueTuple<int, int>>();
         }
         
-        // currPath.Add((1,2)); // Use this syntax to add to the current path
+        currPath.Add((x,y)); // Use this syntax to add to the current path
 
         // TODO Start Problem 5
         // ADD CODE HERE
+        if (maze.IsEnd(x, y))
+        {
+            results.Add(currPath.AsString());
+            currPath.RemoveAt(currPath.Count - 1);
+            return;
+        }
+
 
         // results.Add(currPath.AsString()); // Use this to add your path to the results array keeping track of complete maze solutions when you find the solution.
     }
