21b3122ea2a5abe2c6c1b8e8f53df9596cfcf435
minnie-star <minenhleminnie8@gmail.com>
2025-11-24 22:06:02 +0200

    week o5

--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -127,7 +127,7 @@ public static class Recursion
         }
 
         // Solve using recursion
-        decimal ways = CountWaysToClimb(s - 1) + CountWaysToClimb(s - 2) + CountWaysToClimb(s - 3);
+        decimal ways = CountWaysToClimb(s - 1, remember) + CountWaysToClimb(s - 2, remember) + CountWaysToClimb(s - 3, remember);
         remember[s] = ways;
         return ways;
     }
